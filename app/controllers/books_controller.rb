06eb6aad74bdad6eb7af6781bd87e02e7564255d
EC2 Default User <ec2-user@ip-172-31-13-209.ec2.internal>
2022-02-13 13:55:18 +0000

    [Add]　Log in フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -34,7 +34,7 @@ class BooksController < ApplicationController
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    redirect_to '/books'
+    redirect_to book_path(@book)
   end
 
 
