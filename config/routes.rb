3bfe7370eee468f28305369f4ce8197f62227421
EC2 Default User <ec2-user@ip-172-31-13-209.ec2.internal>
2022-02-13 11:41:57 +0000

    [Add] bookのdestroy機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   resources :books
 
-  resources :users, only: [:show, :edit, :create, :index]
+  resources :users, only: [:show, :edit, :create, :index, :update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
