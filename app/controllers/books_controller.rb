7e6afed3f6697205eeacac8d0c33b6d19ffaba88
EC2 Default User <ec2-user@ip-172-31-13-209.ec2.internal>
2022-02-14 05:54:11 +0000

    [Add]　バリデーション　投稿更新失敗

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -37,7 +37,9 @@ class BooksController < ApplicationController
     @book =Book.find(params[:id])
     if @book.update(book_params)
       flash[:notice] = "You have created book successfully."
-      redirect_to book_path(@book.id)
+      redirect_to book_path(book.id)
+    else
+      render :edit
     end 
   end
   
